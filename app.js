baf9e9596b9744f4af5972cea51b315c8226397f
Antoine Ho <antoineho6@gmail.com>
2019-01-11 19:05:52 -0500

    Can convert int to binary

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-var input = document.getElementById('input-field');
+// Global vars
+let input = document.getElementById('input-field');
 
 // Execute a function when the user releases a key on the keyboard
 input.addEventListener("keyup", function(event) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Number 13 is the "Enter" key on the keyboard
     if (event.key === 'Enter') {
-        // Trigger the button element with a click
-        let number = document.getElementById("input-field").value;
-        if (isNaN(number)){
-            console.log('not a number');
+        let sNumber = document.getElementById("input-field").value;
+        if (isNaN(sNumber)){
+            console.log('not a number');        // temp
         }
         else{
-            console.log('is a number!');
-        }
+            let number = Number(sNumber);
+            let inBinary = '';
 
+            if (isInt(number)){
+                while (number > 0) {
+                    number = number / 2;
+                    if (isFloat(number)){
+                        // A remainder means we remove it and append a 1 to the binary number
+                        number -= 0.5;
+                        inBinary = '1' + inBinary;
+                    }
+                    else {
+                        inBinary = '0' + inBinary;
+                    }
+                }
+                console.log('Num in binary: ' + inBinary);
+            }
+            else {
+                console.log('Need a whole number');
+            }
+        }
     }
 });
+
+function isInt(n){
+    return n % 1 === 0;
+}
+
+function isFloat(n){
+    return n % 1 !== 0;
+}
